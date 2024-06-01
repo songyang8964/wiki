681890a49fc52309ffb01d32b77f7e8c55c06a17
Song Yang <tianhuilin45@gmail.com>
2024-06-01 19:29:04 +0100

    made the-header and the-footer components

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,39 +1,29 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1" @click="navigateTo('/')">Home</a-menu-item>
-        <a-menu-item key="2" @click="navigateTo('/about')">About</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <a-layout-content style="padding: 0 50px">
       <a-layout style="padding: 24px 0; background: #fff">
-        <router-view />
+        <router-view/>
       </a-layout>
     </a-layout-content>
-    <a-layout-footer style="text-align: center">
-      Ant Design ©2018 Created by Ant UED
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
 
-const router = useRouter();
+<script lang="ts">
+import {defineComponent} from 'vue';
+import TheHeader from "@/components/the-header.vue";
+import TheFooter from "@/components/the-footer.vue";
 
-const selectedKeys1 = ref<string[]>(['1']);
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+});
 
-function navigateTo(path: string) {
-  router.push(path);
-}
 </script>
 
 <style scoped>
