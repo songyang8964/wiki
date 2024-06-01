99ed3cdc4f077a7f4497b586dd0ebd966df17637
Song Yang <tianhuilin45@gmail.com>
2024-06-01 18:05:22 +0100

    integrated Ant Design Vue

---
+++ web/src/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+
+    // If there are a lot of pages use lazy loading
+    // to reduce the size of the initial package
+
+
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
